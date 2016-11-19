73d14022c3014296e71a7bb08b8a86c0fed7c0e6
MV <m.vehar@gmail.com>
2016-11-19 18:29:25 +0100

    Compile without openMP

---
+++ soundtouch/source/Android-lib/obj/local/arm64-v8a/objs/soundtouch/__/__/SoundTouch/PeakFinder.o.d
@@ -0,0 +1,50 @@
+/home/mvehar/git/rzp-voice-changer/soundtouch/source/Android-lib/obj/local/arm64-v8a/objs/soundtouch/__/__/SoundTouch/PeakFinder.o: \
+  /home/mvehar/git/rzp-voice-changer/soundtouch/source/Android-lib/jni/../../SoundTouch/PeakFinder.cpp \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/math.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/assert.h \
+  /home/mvehar/git/rzp-voice-changer/soundtouch/source/Android-lib/jni/../../SoundTouch/PeakFinder.h
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/math.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/assert.h:
+
+/home/mvehar/git/rzp-voice-changer/soundtouch/source/Android-lib/jni/../../SoundTouch/PeakFinder.h:
---
+++ soundtouch/source/Android-lib/obj/local/mips/objs/soundtouch/__/__/SoundTouch/sse_optimized.o.d
@@ -0,0 +1,308 @@
+/home/mvehar/git/rzp-voice-changer/soundtouch/source/Android-lib/obj/local/mips/objs/soundtouch/__/__/SoundTouch/sse_optimized.o: \
+  /home/mvehar/git/rzp-voice-changer/soundtouch/source/Android-lib/jni/../../SoundTouch/sse_optimized.cpp \
+  /home/mvehar/git/rzp-voice-changer/soundtouch/source/Android-lib/jni/../../SoundTouch/cpu_detect.h \
+  ../../../include/STTypes.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stdexcept \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_prolog.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/debug/_debug.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_stdexcept.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_stdexcept_base.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_exception.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/../../gabi++/include/exception \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl//gabi++/include/gabixx_config.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cstring.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/string.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/using/cstring \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_fwd.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_iosfwd.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/char_traits.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cstddef.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stddef.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_construct.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_new.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/../../gabi++/include/new \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/cstddef \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/../../gabi++/include/cstddef \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/exception \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_iterator_base.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ptrs_specialize.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/type_traits.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/type_manips.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_move_construct_fwk.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cwchar.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_mbstate_t.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/string \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_alloc.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cstdlib.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stdlib.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cmath.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/math.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_algobase.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/climits \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/limits.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_pair.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_algobase.c \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_function_base.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_iterator.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_alloc.c \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_uninitialized.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_base.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_npos.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_operators.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string.c \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ctraits_fns.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_function.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_function_adaptors.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_range_errors.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_hash.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_hash_fun.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_io.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ostream.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ios.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ios_base.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_locale.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_threads.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/pthread.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_threads.c \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ctime.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/time.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_facets_fwd.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ctype.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/c_locale.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_numpunct.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ios.c \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_streambuf.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_streambuf.c \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ostreambuf_iterator.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ostream.c \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_num_put.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_iostream_string.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_num_put.c \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_limits.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/cfloat \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/float.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_limits.c \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_istream.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_istreambuf_iterator.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_istream.c \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_num_get.h \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_num_get.c \
+  /home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_io.c
+
+/home/mvehar/git/rzp-voice-changer/soundtouch/source/Android-lib/jni/../../SoundTouch/cpu_detect.h:
+
+../../../include/STTypes.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stdexcept:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_prolog.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/debug/_debug.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_stdexcept.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_stdexcept_base.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_exception.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/../../gabi++/include/exception:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl//gabi++/include/gabixx_config.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cstring.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/string.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/using/cstring:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_fwd.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_iosfwd.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/char_traits.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cstddef.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stddef.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_construct.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_new.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/../../gabi++/include/new:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/cstddef:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/../../gabi++/include/cstddef:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/exception:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_iterator_base.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ptrs_specialize.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/type_traits.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/type_manips.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_move_construct_fwk.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cwchar.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_mbstate_t.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/string:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_alloc.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cstdlib.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stdlib.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_cmath.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/math.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_algobase.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/climits:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/limits.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_pair.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_algobase.c:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_function_base.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_iterator.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_alloc.c:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_uninitialized.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_base.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_npos.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_operators.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string.c:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ctraits_fns.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_function.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_function_adaptors.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_range_errors.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_hash.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_hash_fun.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_io.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ostream.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ios.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ios_base.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_locale.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_threads.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/pthread.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_threads.c:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ctime.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/time.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_facets_fwd.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ctype.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/c_locale.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_numpunct.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ios.c:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_streambuf.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_streambuf.c:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ostreambuf_iterator.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_ostream.c:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_num_put.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_iostream_string.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_num_put.c:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_limits.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/cfloat:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/float.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_limits.c:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_istream.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_istreambuf_iterator.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_istream.c:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_num_get.h:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_num_get.c:
+
+/home/mvehar/android-ndk-r13b/sources/cxx-stl/stlport/stlport/stl/_string_io.c:
