daef87820db663701086f1b776e763a97433e2f3
Matej Vehar <matej.vehar@halcom.si>
2016-11-26 02:49:45 +0100

    Removing boiler plate code.

--- VoiceChanger/app/src/main/java/com/vehar/soundtouchandroid/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.vehar.soundtouchandroid;
-
-public class Constants
-{
-	public static final int BUFFER_SIZE_PUT = 4096;
-	public static final int BUFFER_SIZE_GET = 4096;
-	public static final int BUFFER_SIZE_TRACK = 131072;
-	public static final int MAX_CHUNK_SIZE = 8192;
-}
--- VoiceChanger/app/src/main/java/com/vehar/soundtouchandroid/Mp3Decoder.java
+++
@@ -1,8 +0,0 @@
-package com.vehar.soundtouchandroid;
-
-
-public interface Mp3Decoder
-{
-	public byte[] decodeChunk() throws DecoderException;
-	public void close();
-}
